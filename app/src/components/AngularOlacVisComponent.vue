c6654ecaad03ec0a2dfdab6b9e4b6a5de96c3f15
Marco La Rosa <m@lr.id.au>
2018-08-30 15:19:27 +1000

    fix indentation

--- app/src/components/AngularOlacVisComponent.vue
+++ app/src/components/AngularOlacVisComponent.vue
@@ -1,29 +1,29 @@
 <template>
-        <el-card class="box-card style-card" shadow="never">
-            <div slot="header" class="clearfix">
-                <h4>Language Archives data visualisation</h4>
-            </div>
-            <p>
-                The <a href="http://www.language-archives.org/" target="_blank">Open Language Archives Community</a>
-                is an international partnership of institutions and individuals who are creating a worldwide virtual 
-                library of language resources by: (i) developing consensus on best current practice for the digital 
-                archiving of language resources, and (ii) developing a network of interoperating repositories and 
-                services for housing and accessing such resources.
-            </p>
-            <p>
-                This project was a first attempt at answering the question: Can we simplify the discovery of those
-                languages that do not have manuy resources?
-            </p>
-            <p>
-                - Use the application @ <a href="http://language-archives.services/olacvis/" target="_blank">http://language-archives.services/olacvis/</a><br/>
-                - The source code is accessible @ <a href="https://github.com/marcolarosa/pdsc-olac-visualisation" target="_blank">https://github.com/marcolarosa/pdsc-olac-visualisation</a>
-            </p>
-            <div class="outer ">
-                <img src="src/assets/images/olacvis.jpg" class="style-image position-image1">
-                <img src="src/assets/images/olacvis1.jpg" class="style-image position-image2">
-                <img src="src/assets/images/olacvis2.jpg" class="style-image position-image3">
-            </div>
-        </el-card>
+    <el-card class="box-card style-card" shadow="never">
+        <div slot="header" class="clearfix">
+            <h4>Language Archives data visualisation</h4>
+        </div>
+        <p>
+            The <a href="http://www.language-archives.org/" target="_blank">Open Language Archives Community</a>
+            is an international partnership of institutions and individuals who are creating a worldwide virtual 
+            library of language resources by: (i) developing consensus on best current practice for the digital 
+            archiving of language resources, and (ii) developing a network of interoperating repositories and 
+            services for housing and accessing such resources.
+        </p>
+        <p>
+            This project was a first attempt at answering the question: Can we simplify the discovery of those
+            languages that do not have manuy resources?
+        </p>
+        <p>
+            - Use the application @ <a href="http://language-archives.services/olacvis/" target="_blank">http://language-archives.services/olacvis/</a><br/>
+            - The source code is accessible @ <a href="https://github.com/marcolarosa/pdsc-olac-visualisation" target="_blank">https://github.com/marcolarosa/pdsc-olac-visualisation</a>
+        </p>
+        <div class="outer ">
+            <img src="src/assets/images/olacvis.jpg" class="style-image position-image1">
+            <img src="src/assets/images/olacvis1.jpg" class="style-image position-image2">
+            <img src="src/assets/images/olacvis2.jpg" class="style-image position-image3">
+        </div>
+    </el-card>
 </template>
 
 <script>
