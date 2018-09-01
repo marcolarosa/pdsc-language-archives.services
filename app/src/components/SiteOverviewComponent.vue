b1928f6fcb5d2cb44cbafbc86de7cc0b5eeaf1f5
Marco La Rosa <m@lr.id.au>
2018-09-01 16:08:55 +1000

    cleanup; adjust link color; styling tweaks

--- app/src/components/SiteOverviewComponent.vue
+++ app/src/components/SiteOverviewComponent.vue
@@ -41,7 +41,7 @@
                     <em>
                         <strong>
                             The projects shown in this site are led by <a href="http://nthieberger.net" target="_blank">Nick Thieberger</a> and 
-                            have been funded as part of the
+                            have been funded as part of the<br/>
                             ARC Centre of Excellence for the Dynamics of Language (ARC grant CE140100041). 
                         </strong>
                     </em>
