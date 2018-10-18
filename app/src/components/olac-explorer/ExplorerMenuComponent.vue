5c25e350abc1b852557bf686f0a76f60d476ef14
Marco La Rosa <m@lr.id.au>
2018-10-18 15:36:59 +1100

    Add logo and application title

--- app/src/components/olac-explorer/ExplorerMenuComponent.vue
+++ app/src/components/olac-explorer/ExplorerMenuComponent.vue
@@ -1,5 +1,19 @@
 <template>
     <el-menu default-active="1" :router="true" class="style-menu">
+        <div class="row">
+            <div class="col text-center py-2">
+                <router-link to="/">
+                    <img src="assets/images/coedl-logo.png" class="style-logo">
+                </router-link>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col text-center py-2">
+                Language Archives<br /> Explorer
+            </div>
+        </div>
+        <hr />
         <el-menu-item index="1" route="/olac-explorer/browse-country">
             <span>Browse Country Data</span>
         </el-menu-item>
@@ -20,5 +34,9 @@ export default {
     left: 0;
     height: 100vh;
     min-width: 220px;
+}
+
+.style-logo {
+    width: 200px;
 }
 </style>
