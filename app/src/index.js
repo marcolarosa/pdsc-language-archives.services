75993edaab3995b35f560da528ed0f1f0030de6b
Marco La Rosa <m@lr.id.au>
2019-07-16 09:55:28 +1000

    add analytics

--- app/src/index.js
+++ app/src/index.js
@@ -5,6 +5,7 @@ import ElementUI from "element-ui";
 import locale from "element-ui/lib/locale/lang/en";
 import VueScrollTo from "vue-scrollto";
 import { VueMasonryPlugin } from "vue-masonry";
+import VueAnalytics from "vue-analytics";
 Vue.use(ElementUI, { locale });
 Vue.use(VueScrollTo);
 Vue.use(VueMasonryPlugin);
@@ -12,6 +13,16 @@ Vue.use(VueMasonryPlugin);
 import App from "components/app.vue";
 import { router } from "routes";
 import { store } from "store";
+
+const isProd = process.env.NODE_ENV === "production";
+Vue.use(VueAnalytics, {
+    id: "UA-36256674-3",
+    router,
+    debug: {
+        isProd: !isProd,
+        sendHitTask: isProd
+    }
+});
 
 // Vue.prototype.inteja = { configuration };
 App.router = router;
